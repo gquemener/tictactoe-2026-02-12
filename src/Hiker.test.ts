af33c025dd513cf3feef8acb52bf8c1e73c9beb0
Joel Piazzalunga-Lerat <joel.piazzalungalerat-ext@nickel.eu>
2026-02-12 13:43:15 +0100

    Implement the cell feature
    commit dc8d29b6d8e04c406da9ea46365c475f50dfb664
    Author: Gildas Quéméner <gildas.quemener@gmail.com>
    Date:   Thu Feb 12 13:41:58 2026 +0100
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:src/Hiker.test.ts
    
    commit f049f98e10ef0964409dff04d8297a4506863434
    Author: Joel Piazzalunga-Lerat <joel.piazzalungalerat-ext@nickel.eu>
    Date:   Thu Feb 12 13:36:08 2026 +0100
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:src/Hiker.test.ts
    
    commit b2f06a82da9e9f686147db35550cbc8decb8cb35
    Author: Joel Piazzalunga-Lerat <joel.piazzalungalerat-ext@nickel.eu>
    Date:   Thu Feb 12 13:34:17 2026 +0100
    
        no message
    
    commit 1f6fb268a16da67f76a4db332eb2427df2913749
    Author: Gildas Quéméner <gildas.quemener@gmail.com>
    Date:   Thu Feb 12 13:25:54 2026 +0100
    
        Make test pass
    
    commit 905b7bcc4b39379ca82638147ec48f201c2053e4
    Author: Joel Piazzalunga-Lerat <joel.piazzalungalerat-ext@nickel.eu>
    Date:   Thu Feb 12 13:20:32 2026 +0100
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:src/Hiker.test.ts
    
    commit b2a5b1e227a4bc6b57dcac4fc8d2a6f68712a78a
    Author: Gildas Quéméner <gildas.quemener@gmail.com>
    Date:   Thu Feb 12 13:14:15 2026 +0100
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:src/Hiker.test.ts
    
    commit 4e73a8ac236948a416f5fc048306536c2e07d47a
    Author: Gildas Quéméner <gildas.quemener@gmail.com>
    Date:   Thu Feb 12 12:59:10 2026 +0100
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:.nvmrc
    
    Co-authored-by: Gildas Quéméner <gildas.quemener@gmail.com>

--- src/Hiker.test.ts
+++ src/Hiker.test.ts
@@ -1,14 +1,55 @@
-import { Hiker } from './Hiker'
-
-describe('Hiker', () => {
-  it('answers 42', () => {
+describe('Cell', () => {
+  it('play player X', () => {
     // Arrange
-    const hiker = new Hiker()
+    const cell: Cell = {
+      content: ''
+    }
 
     // Act
-    const answer = hiker.getAnswer()
+    const newCell: Cell = play('X', cell)
 
     // Assert
-    expect(answer).toBe(42)
+    expect(newCell.content).toBe('X')
+  })
+
+  it('play player O', () => {
+    // Arrange
+    const cell: Cell = {
+      content: ''
+    }
+
+    // Act
+    const newCell: Cell = play('O', cell)
+
+    // Assert
+    expect(newCell.content).toBe('O')
+  })
+  it('should not erase player cell', () => {
+    // Arrange
+    const cell: Cell = {
+      content: 'X'
+    }
+
+    // Act
+    const newCell: Cell = play('O', cell)
+
+    // Assert
+    expect(newCell.content).toBe('X')
   })
 })
+
+type Player = 'X' | 'O';
+const play = (player: Player, cell: Cell): Cell => {
+  if (cell.content === '') {
+    return {
+      content: player
+    };
+  }
+  return cell;
+}
+
+type EMPTY = ''
+
+type Cell = {
+  content: EMPTY | Player
+}
