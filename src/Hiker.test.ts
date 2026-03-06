2fe20937039ef6a8a0fb6abae417a37f63c58c4e
Joel Piazzalunga-Lerat <joel.piazzalungalerat-ext@nickel.eu>
2026-03-06 13:18:51 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/Hiker.test.ts

--- src/Hiker.test.ts
+++ src/Hiker.test.ts
@@ -1,5 +1,6 @@
 
-// Given a client makes a deposit of 1000 on 10-01-2012
+// Given a client
+// makes a deposit of 1000 on 10-01-2012
 // And a deposit of 2000 on 13-01-2012
 // And a withdrawal of 500 on 14-01-2012
 // When she prints her bank statement
@@ -13,5 +14,14 @@ describe('BankingAccount', () => {
   it('test', () => {
     var account = new BankingAccount();
     account.deposit(1000, "10-01-2012");
+    account.deposit(2000, "13-01-2012");
+    account.withdrawal(500, "14-01-2012");
+    account.printStatement()
+    expect(screen.printLn).toHave([
+        "date || credit || debit || balance",
+        "14/01/2012 || || 500.00 || 2500.00",
+        "13/01/2012 || 2000.00 || || 3000.00",
+        "10/01/2012 || 1000.00 || || 1000.00"
+    ])
   })
 })
